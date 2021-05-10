97b3254c6c8fba38ae8e1ef66888686e1d007b46
Anurag Singh <anuragsinghbam@gmail.com>
2021-05-10 14:50:52 +0530

    add anurag

--- horse_racing/static/assets/js/phone-auth.js
+++ horse_racing/static/assets/js/phone-auth.js
@@ -1,71 +1,83 @@
 var config = {
-    "apiKey": "AIzaSyAVoCvlXSDlY49LyQAkZeWhNjzp2VWV1Os",
-    "authDomain": "tokyo-kingdom-299513.firebaseapp.com",
-    "projectId": "tokyo-kingdom-299513",
-    "storageBucket": "tokyo-kingdom-299513.appspot.com",
-    "messagingSenderId": "585339205310",
-    "appId": "1:585339205310:web:055d44f7a9ef0eee22d0ef",
-    "measurementId": "G-HC4JTP0D9N",
-    "databaseURL": ""
+    apiKey: 'AIzaSyAVoCvlXSDlY49LyQAkZeWhNjzp2VWV1Os',
+    authDomain: 'tokyo-kingdom-299513.firebaseapp.com',
+    projectId: 'tokyo-kingdom-299513',
+    storageBucket: 'tokyo-kingdom-299513.appspot.com',
+    messagingSenderId: '585339205310',
+    appId: '1:585339205310:web:055d44f7a9ef0eee22d0ef',
+    measurementId: 'G-HC4JTP0D9N',
+    databaseURL: '',
+  }
+  
+  
+console.log('Anurag'); 
+firebase.initializeApp(config)
+
+window.onload = function () {
+  render()
+  document.getElementById('otp-input-block').style.display = 'none'
+  document.getElementById('signup-btn').style.display = 'none'
+}
+function render() {
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    'recaptcha-container',
+    {
+      size: 'invisible',
+      callback: (response) => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        onSignInSubmit()
+      },
+    }
+  )
 }
 
-firebase.initializeApp(config);
+function phoneAuth() {
+  var number = document.getElementById('mob').value
+  number = '+91' + number
 
-window.onload = function(){
-    render();
-    document.getElementById('otp-input-block').style.display = 'none';
-    document.getElementById('signup-btn').style.display = 'none';
-};
-function render(){
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
-        'size': 'invisible',
-        'callback': (response) => {
-            // reCAPTCHA solved, allow signInWithPhoneNumber.
-            onSignInSubmit();
-        }
-    });
-}
-
-function phoneAuth(){
-    var number = document.getElementById('mob').value;
-    number = '+91'+number;
-
-    firebase.auth().signInWithPhoneNumber(number, recaptchaVerifier)
+  firebase
+    .auth()
+    .signInWithPhoneNumber(number, recaptchaVerifier)
     .then((confirmationResult) => {
       // SMS sent. Prompt user to type the code from the message, then sign the
       // user in with confirmationResult.confirm(code).
-      window.confirmationResult = confirmationResult;
-      codeResult = confirmationResult;
-      console.log(codeResult);
-      alert("message sent check your phone!");
-    }).catch((error) => {
+      window.confirmationResult = confirmationResult
+      codeResult = confirmationResult
+      console.log(codeResult)
+      alert('message sent check your phone!')
+    })
+    .catch((error) => {
       // Error; SMS not sent
       // ...
-      document.getElementById('msg').innerHTML = "Enter only 10 digit Number without +91";
-      console.log(error);
-    });
+      document.getElementById('msg').innerHTML =
+        'Enter only 10 digit Number without +91'
+      console.log(error)
+    })
 }
 
-function codeVerify(){
-    var otp = document.getElementById('verificationCode').value;
-    codeResult.confirm(otp)
+function codeVerify() {
+  var otp = document.getElementById('verificationCode').value
+  codeResult
+    .confirm(otp)
     .then((result) => {
       // User signed in successfully.
-      const user = result.user;
-      document.forms['register'].submit();
-    }).catch((error) => {
+      const user = result.user
+      document.forms['register'].submit()
+    })
+    .catch((error) => {
       // User couldn't sign in (bad verification code?)
       // ...
-      document.getElementById('msg').innerHTML = "Something went wrong! Try again";
-    });
+      document.getElementById('msg').innerHTML =
+        'Something went wrong! Try again'
+    })
 }
 
-function enable(){
-    document.getElementById('otp-input-block').style.display = 'block';
-    document.getElementById('get-otp').disabled = false;
+function enable() {
+  document.getElementById('otp-input-block').style.display = 'block'
+  document.getElementById('get-otp').disabled = false
 }
 
-function enable_signup(){
-    document.getElementById('get-otp').disabled = true;
-    document.getElementById('signup-btn').style.display = "block";
+function enable_signup() {
+  document.getElementById('get-otp').disabled = true
+  document.getElementById('signup-btn').style.display = 'block'
 }
